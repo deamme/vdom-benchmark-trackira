7d4fe0eb2b11569aa1e2c533bba28d7e6e8ba3dd
Deam <deamme@users.noreply.github.com>
2015-07-02 14:54:06 +0200

    minor fix

--- web/js/main.js
+++ web/js/main.js
@@ -1,53 +1,52 @@
 'use strict';
 
 var benchmark = require('vdom-benchmark-base');
-var t = require('trackira');
-
+var t = require('./trackira.js');
+console.log(t);
 var NAME = 'Trackira';
 var VERSION = t.version;
 
 function renderTree(nodes) {
-  var children = [];
-  var i;
-  var e;
-  var n;
+    var children = [];
+    var i;
+    var e;
+    var n;
 
-  for (i = 0; i < nodes.length; i++) {
-    n = nodes[i];
-    if (n.children !== null) {
-      children.push(new t.Element('div', {key: n.key}, renderTree(n.children)));
-    } else {
-      children.push(new t.Element('span', {key: n.key}, [new t.Text(n.key.toString())]));
+    for (i = 0; i < nodes.length; i++) {
+        n = nodes[i];
+        if (n.children !== null) {
+            children.push(new t.Element('div', {key: n.key}, renderTree(n.children)));
+        } else {
+            children.push(new t.Element('span', {key: n.key}, [new t.Text(n.key.toString())]));
+        }
     }
-  }
 
-  return children;
+    return children;
 }
 
 function BenchmarkImpl(container, a, b) {
-  this.container = container;
-  this.a = a;
-  this.b = b;
-  this._node = null;
+    this.container = container;
+    this.a = a;
+    this.b = b;
+    this._node = null;
 }
 
 BenchmarkImpl.prototype.setUp = function() {
 };
 
 BenchmarkImpl.prototype.tearDown = function() {
-  t.detach(this.container);
+    this._node.destroy();
 };
 
 BenchmarkImpl.prototype.render = function() {
-  this._node = new t.Element('div', null, renderTree(this.a));
-  this._node.render(this.container);
+    this._node = new t.Element('div', null, renderTree(this.a));
+    this._node.render();
 };
 
 BenchmarkImpl.prototype.update = function() {
-  var newNode = new t.Element('div', {}, renderTree(this.b));
-  this._node = t.patch(this._node, newNode);
+    t.patch.node(this._node, new t.Element('div', {}, renderTree(this.b)));
 };
 
 document.addEventListener('DOMContentLoaded', function(e) {
-  benchmark(NAME, VERSION, BenchmarkImpl);
+    benchmark(NAME, VERSION, BenchmarkImpl);
 }, false);
