4757ebe06a7c3c33fa469a716625c8daffb716fa
Deam <deamme@users.noreply.github.com>
2015-07-16 09:52:49 +0200

    update

--- web/js/main.js
+++ web/js/main.js
@@ -1,52 +1,56 @@
-'use strict';
-
 var benchmark = require('vdom-benchmark-base');
-var t = require('trackira');
+var Trackira = require('trackira');
+var Element = Trackira.Element;
+var Text = Trackira.Text;
+var patch = Trackira.patch;
 
 var NAME = 'Trackira';
-var VERSION = t.version;
+var VERSION = Trackira.version;
 
 function renderTree(nodes) {
-    var children = [];
-    var i;
-    var e;
-    var n;
+  var children = [];
+  var i;
+  var n;
 
-    for (i = 0; i < nodes.length; i++) {
-        n = nodes[i];
-        if (n.children !== null) {
-            children.push(new t.Element('div', {key: n.key}, renderTree(n.children)));
-        } else {
-            children.push(new t.Element('span', {key: n.key}, [new t.Text(n.key.toString())]));
-        }
+  for (i = 0; i < nodes.length; i++) {
+    n = nodes[i];
+    if (n.children !== null) {
+      children.push(new Element("div", { key: n.key }, renderTree(n.children)));
+    } else {
+      children.push(new Element("span", { key: n.key }, [new Text(n.key.toString())]));
     }
+  }
 
-    return children;
+  return children;
 }
 
 function BenchmarkImpl(container, a, b) {
-    this.container = container;
-    this.a = a;
-    this.b = b;
-    this._node = null;
+  this.container = container;
+  this.a = a;
+  this.b = b;
+  this._node = null;
+  this._root = null;
 }
 
 BenchmarkImpl.prototype.setUp = function() {
 };
 
 BenchmarkImpl.prototype.tearDown = function() {
-    this._node.destroy();
+  this._node.detach();
 };
 
 BenchmarkImpl.prototype.render = function() {
-    this._node = new t.Element('div', null, renderTree(this.a));
-    this._node.render();
+  this._node = new Element("div", {}, renderTree(this.a));
+  this._root = this._node.render();
+  this.container.appendChild(this._root);
 };
 
 BenchmarkImpl.prototype.update = function() {
-    t.patch(this._node, new t.Element('div', {}, renderTree(this.b)));
+  var newNode = new Element("div", {}, renderTree(this.b));
+  this._root = this._node.patch(newNode);
+  this._node = newNode;
 };
 
 document.addEventListener('DOMContentLoaded', function(e) {
-    benchmark(NAME, VERSION, BenchmarkImpl);
+  benchmark(NAME, VERSION, BenchmarkImpl);
 }, false);
